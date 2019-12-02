7d4773eb99425bb7922b94df1f1278fc611b18b6
franzosterkamp <franz.osterkamp@gmx.net>
2019-12-02 14:52:23 +0100

    add new functions and create passwordfile

--- utils/cmds.js
+++ utils/cmds.js
@@ -1,18 +1,35 @@
-const passwords = {
-  wifi: 123,
-  mac: "mac444"
-};
+const fs = require("fs");
+
+function read() {
+  try {
+    const jsonPw = fs.readFileSync("passwords.json", "utf-8");
+    const passwords = JSON.parse(jsonPw);
+    return passwords;
+  } catch (error) {
+    return {};
+  }
+}
+
+function write(passwords) {
+  const data = JSON.stringify(passwords, null, 2);
+  fs.writeFileSync(`passwords.json`, data);
+}
 
 function get(key) {
+  const passwords = read();
   return passwords[key];
 }
 
 function set(key, value) {
+  const passwords = read();
   passwords[key] = value;
+  write(passwords);
 }
 
 function unset(key) {
+  const passwords = read();
   delete passwords[key];
+  write(passwords);
 }
 
 exports.get = get;
